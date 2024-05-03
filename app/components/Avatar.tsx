867df9934eadf0e02495d33e907fb48ab4f31c42
sadeghweb <hashemisadegh59@yahoo.com>
2024-05-04 00:04:37 +0330

    getUsers,userslist&&more

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -11,11 +11,12 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
     <div className="relative">
       <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
         <Image
-          src={user?.image || "/images/placeholder-2.png"}
+          src={user?.image || "/images/placeholder.png"}
           alt="avatar"
           fill
         />
       </div>
+      <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3" />
     </div>
   );
 };
